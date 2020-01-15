557c4829674f2c0431840a5587b16ad6e9894c1f
Daniel Graham <daniel.dev2019@gmail.com>
2020-01-15 15:02:18 +0100

    Done

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <v-app>
-    
+    <div>
+      <v-toolbar dark prominent src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Color Test</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-export</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </div>
+
     <v-content>
       <router-view></router-view>
     </v-content>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,53 @@
 <template>
   <div class="home">
-    <h1>Homepage</h1>
     
-    <v-btn class="pink white--text">click me</v-btn>
-    <v-btn depressed color="pink">click me</v-btn>
-    <v-btn text color="pink">click me</v-btn>
+    <v-container class="py-5">
+      <v-row class="d-flex justify-center my-5 py-5">
 
-    <v-btn depressed class="pink white--text">
-      <v-icon left>email</v-icon>
-      <span>email me</span>
-    </v-btn>
+        <v-col class="ma-5 pa-5 d-flex justify-center">
+          <v-btn @click="clickedColor('green')" height=70px class="display-1 green white--text font-weight-bold">click me</v-btn>
+    
+        </v-col>
 
-    <v-btn depressed small class="pink white--text">
-      <v-icon left small>email</v-icon>
-      <span>email me</span>
-    </v-btn>
+        <v-col class="ma-5 pa-5 d-flex justify-center">
+          <v-btn @click="clickedColor('red')" height=70px class="display-1 red white--text font-weight-bold">click me</v-btn>
+    
+        </v-col>
 
-    <v-btn depressed large class="pink white--text">
-      <span>email me</span>
-      <v-icon right large>email</v-icon>
-    </v-btn>
+        <v-col class="ma-5 pa-5 d-flex justify-center">
+          <v-btn @click="clickedColor('yellow')" height=70px class="display-1 yellow font-weight-bold">click me</v-btn>
+        </v-col>
 
-    <v-btn fab depressed small dark color="purple">
-      <v-icon>favorite</v-icon>
-    </v-btn>
-  
+      </v-row>
+
+      <div class="row ma-5 pa-5"></div>
+      <div class="row ma-5 pa-5"></div>
+      
+      <!-- <hr class="blue lighten-4 mt-5"> -->
+      
+      <div class="mt-5 border border-blue">
+
+        <v-row height="150px" v-show="green" :class="['green', {visible: visible}]">
+          
+          <v-btn text v-show="!closed" @click="closeIt"><v-icon class="white--text">close</v-icon></v-btn>
+          
+          
+        </v-row>
+
+        <v-row height="150px" v-show="red" :class="['red', {visible: visible}]">
+
+          <v-btn text v-show="!closed" @click="closeIt"><v-icon class="white--text">close</v-icon></v-btn>
+
+        </v-row>
+
+        <v-row height="150px" v-show="yellow" :class="['yellow', {visible: visible}]">
+          <v-btn text v-show="!closed" @click="closeIt"><v-icon>close</v-icon></v-btn>
+        </v-row>
+
+      </div>
+      
+    </v-container>
+
   </div>
 </template>
 
@@ -35,6 +58,46 @@ export default {
   name: 'home',
   components: {
 
-  }
+  },
+  data() {
+    return {
+      green: false,
+      red: false,
+      yellow: false,
+      closed: false,
+      visible: ""
+    }
+  },
+  methods: {
+    
+    clickedColor(color) {
+      if (color === "green") {
+        this.green = true
+        this.red = false
+        this.yellow= false
+        this.closed = false
+      } else if (color === "red") {
+        this.red = true
+        this.green = false
+        this.yellow= false
+        this.closed = false
+      } else if (color === "yellow") {
+        this.green = false
+        this.red = false
+        this.yellow= true
+        this.closed = false
+      }
+    },
+    
+    closeIt() {
+      if (!this.closed) {
+        this.closed = true
+        this.visible = "d-none"
+      } else {
+        this.closed = false
+        this.visible = ""
+      }
+    },
+  },
 }
 </script>
